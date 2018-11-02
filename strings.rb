38eafa9a0cf525d1b3136998c10a01481f79c94a
Luis Orozco <luisorozco@Eduardos-MBP.localdomain>
2018-11-02 12:26:54 -0600

    strings.rb file edited.

--- strings.rb
+++ strings.rb
@@ -62,4 +62,52 @@ p "Real Madrid".slice(-2)
 p "Real Madrid is the best team in the world"[5, 6]
 p "Real Madrid is the best team in the world".slice(5, 6)
 
+# Extracting several characters using range
+# Using .. will include the last character in the extraction
+# Using ... won't include the last character in the extraction
+# If we exceed the size of the string in the second argument of the range,
+# ruby will automatically stop extracting characters in the last character
+# of the string, even if we use the ...
+# We can use negative numbers as well
+
+p "Real Madrid Club de Futbol"[5..25]
+p "Hey there".slice(4...8)
+p "I like playing soccer a lot"[7...1000]
+p "Ruby is a great language".slice(10..-10)
+
+# Strings in ruby ara mutable
+
+str = "I love traveling"
+
+puts str
+
+str[2,4] = "like"
+
+puts str
+
+# Some String methods
+
+p "first letter gets uppercase".capitalize
+p "all letters get uppercase".upcase
+p "ALL LETTERS GET LOWERCASE".downcase
+p 'lowErCAse leTTers GET upperCASE and UPercase letteRS get LOWercase'.swapcase
+p "Reverses a string".reverse
+p "CHAINING SEVERAL METHODS".downcase.capitalize.swapcase.reverse
+p "".empty?
+p "".nil?
+
+# Bang methods, these methods have side effects, this means they mutate the string
+# we turn a method into a Bang method by adding a ! symbol at the end of the method name
+# we can add the ! symbol to only some methods, not to all
+
+p orStr = "Original string"
+
+orStr.upcase!
+
+p orStr
+
+# include? method returns true if it finds a string we pass as argument
+
+p "Hi, my name is Luis and I love playing soccer.".include?("and")
+
 
